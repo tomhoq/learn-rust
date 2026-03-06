56b9a27a5db87979498105e67323ae4187e0a6ba
tomhoq <tomhoq2003@gmail.com>
2026-03-06 13:37:39 +0100

    edit media

--- media/src/main.rs
+++ media/src/main.rs
@@ -1,10 +1,12 @@
-use std::any::Any;
+>use std::any::Any;
 
 #[derive(Debug)]
 enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String },
+    Podcast(u32),
+    Placeholder,
 }
 
 impl Media {
@@ -15,8 +17,25 @@ impl Media {
                 format!("Movie: {} directed by {}", title, director)
             }
             Media::Audiobook { title } => format!("Audiobook: {}", title),
+            Media::Podcast(id) => format!("Number: {}", id),
+            Media::Placeholder => format!("Placeholder"),
             _ => String::from("No type"),
         }
+    }
+}
+
+#[derive(Debug)]
+struct Catalog {
+    items: Vec<Media>,
+}
+
+impl Catalog {
+    fn new() -> Self {
+        Catalog { items: vec![] }
+    }
+
+    fn add(&mut self, media: Media) {
+        self.items.push(media);
     }
 }
 
@@ -39,7 +58,9 @@ fn main() {
         author: String::from("Mr Jenkins"),
     };
 
-    println!("{}", book.description());
-    println!("{}", movie.description());
-    println!("{}", media.description());
+    let mut catalog = Catalog::new();
+    catalog.add(movie);
+    catalog.add(book);
+    catalog.add(media);
+    println!("{:#?}", catalog);
 }
